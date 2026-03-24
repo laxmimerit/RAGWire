4c3c49017c8a808ce9e647cc2bf87c17029979eb
laxmimerit <laxmimerit@gmail.com>
2026-03-24 19:07:26 +0530

    updated tags

--- ragwire/__init__.py
+++ ragwire/__init__.py
@@ -27,7 +27,7 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("ragwire")
 except PackageNotFoundError:
-    __version__ = "1.2.0"
+    __version__ = "1.2.1"
 
 __author__ = "KGP Talkie Private Limited"
 
